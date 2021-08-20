1168674fe218568b5dfc18dadf738fc8fb595b37
Josepheze <70171734+Josepheze@users.noreply.github.com>
2021-08-20 06:41:56 +0100

    initial commit

---
+++ zuri.php
@@ -0,0 +1,84 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>RESUME</title>
+	<link rel="stylesheet" href="bootstrap.min.css">
+  <script src="jquery-3.5.1.min.js"></script>
+  <script src="popper.min.js"></script>
+  <script src="bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+	<div class="row">
+		<div class="col-sm-4 " style="height:auto; background-color:rgb(0,145,75); "> 
+<img src="joseph.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236" style="margin: 20px,10px,10px,20px; padding-left: 20px; padding-top: 10px; text-align: center; border-style: solid;
+  border-color: red;"> 
+<!--<img src="icons-1.5.0/icons/alt.svg" style="width: 100px;">-->
+<article style=" text-align: center; padding-top: 50px"> contact</article>
+<div>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="26" height="36" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16" style="">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/><span> 261 icda igbogbo lagos</span>
+
+</svg> 
+
+<p style="padding-top: 10px"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="36" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>  <span style="padding-left: 10px">07017573933</span></p>
+<p style="padding-top: 2px"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="36" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+</svg><span style="padding-left: 10px">ezechukwujoseph2017@gmail.com</span></p>
+<aside>
+<h1 style="text-align: center;"> REFERENCES</h1>
+<p> Mr George philip (blogger) </p>
+<p>Owner of betechwise</p>
+<p style="padding-top: 10px"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="36" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>  <span style="padding-left: 10px">07017573933</span></p>
+<p style="padding-top: 2px"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="36" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+</svg><span style="padding-left: 10px">ezechukwujoseph2017@gmail.com</span></p>
+
+</aside>
+</div>
+</div>
+    <div class="col-sm-8" style="background-color: red;">
+      <h1> <?php $_POST['firstname'] ?></h1>
+      <p style="text-underline-position: ">BACKEND DEVELOPER (PHP)</p>
+
+    <div class="jumbotron">
+    	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+</svg><span> COVER LETTER</span>
+    </div>
+
+    <article> asdvk.......;;;;;FKJVDFJ</article>
+    <div class="container">
+    <h1>Registration Form</h1>
+  <h3>Saving Data into the database</h3>
+  <form action="connect.php" method="POST">
+    <label>Firstname</label>
+    <input type="text" placeholder="Enter firstname" name="firstname" required ="true">
+    <br>
+    <label>Lastname</label>
+    <input type="text" placeholder="Enter lastname" name="lastname" required ="true">
+    <br>
+    <label>Email</label>
+    <input type="email" placeholder="Enter email" name="email" required ="true">
+    <br>
+    <label>Phone</label>
+    <input type="phone" placeholder="Enter Phone" name="phone" required ="true">
+    <br> 
+    <button type="submit">Save</button>
+  </form> 
+
+    </div>
+</div>
+  </div>
+</div>
+</body>
+</html> 
